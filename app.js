ab4443d8bb3cb39522d2bce0d536b102559cab8e
Dipak giri <dipakgiri1200@gmail.com>
2022-03-25 00:45:30 +0545

    added a Procfile .gitignore nodeversion to package.json

--- app.js
+++ app.js
@@ -146,7 +146,10 @@ app.get("/about", function(req,res){
   res.render("about");
 });
 
-
-app.listen(3000, function(){
-console.log("Server is running on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function(){
+console.log("Server has started successfully !!");
 });
